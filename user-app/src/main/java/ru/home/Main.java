8f1a35c35ae07922dec94c3ac0e4b139d0a55627
nblackbu <sitdikovaelvinaa96@gmail.com>
2022-12-11 20:27:08 +0300

    11.12

--- user-app/src/main/java/ru/home/Main.java
+++ user-app/src/main/java/ru/home/Main.java
@@ -1,18 +1,10 @@
 package ru.home;
 
-import ru.home.controller.UserController;
-import ru.home.controller.UserControllerImpl;
-import ru.home.dao.UserDaoImpl;
-import ru.home.model.User;
-import ru.home.service.UserServiceImpl;
-import ru.home.ui.WebUserInterface;
-
-import java.util.HashSet;
-import java.util.List;
+import ru.home.ui.WebUserInterfaceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        WebUserInterface webUserInterface = new WebUserInterface();
+        WebUserInterfaceImpl webUserInterface = new WebUserInterfaceImpl();
         System.out.println(webUserInterface.getUser(7L));
     }
 }
--- user-app/src/main/java/ru/home/ui/WebUserInterface.java
+++ user-app/src/main/java/ru/home/ui/WebUserInterface.java
@@ -1,45 +1,15 @@
 package ru.home.ui;
 
-import ru.home.controller.UserController;
-import ru.home.controller.UserControllerImpl;
-import ru.home.dao.UserDaoImpl;
 import ru.home.model.User;
-import ru.home.service.UserServiceImpl;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class WebUserInterface {
-    UserController userController = new UserControllerImpl(new UserServiceImpl(new UserDaoImpl()));
-
-    public User getUser(Long id) {
-        User user = userController.getUser(7l);
-        return user;
-    }
-
-    public List<User> findUserByName(String name) {
-        List<User> users = userController.findUserByName("Bob");
-        return users;
-    }
-
-    public Long createUser(String name, Set<String> citiesLive, Set<String> citiesWork) {
-        User user = new User(5L,"Masha", new HashSet<>(), new HashSet<>());
-        Long id = userController.createUser(name, citiesLive, citiesWork);
-        return id;
-    }
-
-    public List<User> getAllUsers() {
-        List<User> users = userController.getAllUsers();
-        return users;
-    }
-
-    public User updateUser(User user) {
-        User user2 = new User(5L,"Elena", new HashSet<>(), new HashSet<>());
-        return userController.updateUser(user2);
-    }
-
-    public void deleteUser(Long id) {
-        userController.deleteUser(8L);
-    }
+public interface WebUserInterface {
+    User getUser(Long id);
+    List<User> findUserByName(String name);
+    Long createUser(String name, Set<String> citiesLive, Set<String> citiesWork);
+    List<User> getAllUsers();
+    User updateUser(User user);
+    void deleteUser(Long id);
 }
